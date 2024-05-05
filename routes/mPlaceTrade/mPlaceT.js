e0dd3cc41faaf51c2e33813160130be7b3e597d3
Osemu <vincentaimiosior@gmail.com>
2024-05-05 17:07:59 +0000

    updates

--- routes/mPlaceTrade/mPlaceT.js
+++ routes/mPlaceTrade/mPlaceT.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/2(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','..','Properties','marketPlace4.html'))
+    res.sendFile(path.join(__dirname, '..','..','Properties','marketPlace2.html'))
 });
 
+router.get('/3(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','..','Properties','marketPlace3.html'))
+});
+
+router.get('/4(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','..','Properties','marketPlace4.html'))
+});
 
 module.exports = router;
